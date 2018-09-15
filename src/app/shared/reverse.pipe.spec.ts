ff8f627b684dfbc0602138f0f935849e0fd9acfa
nydi <stefan.nydegger@gmail.com>
2018-09-15 12:39:24 +0200

    lecture 393

--- src/app/shared/reverse.pipe.spec.ts
+++ src/app/shared/reverse.pipe.spec.ts
@@ -1,7 +1,6 @@
 import {async} from '@angular/core/testing';
 import {ReversePipe} from './reverse.pipe';
 
-
 it(`should reverse the string 'hello'`, async(() => {
   const reversePipe = new ReversePipe();
   expect(reversePipe.transform('hello')).toEqual('olleh');
